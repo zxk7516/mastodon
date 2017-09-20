678ea41d65d91690205fbdc3557aa51d4bcf509d
Eugen Rochko <eugen@zeonfederated.com>
2017-09-20 03:54:30 +0200

    Fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-lock '3.8.2'
+lock '3.9.1'
 
 set :repo_url, ENV.fetch('REPO', 'https://github.com/tootsuite/mastodon.git')
 set :branch, ENV.fetch('BRANCH', 'master')
 
 set :application, 'mastodon'
 set :rbenv_type, :user
-set :rbenv_ruby, File.read('.ruby-version').strip
+set :rbenv_ruby, '2.4.1'
 set :migration_role, :app
 
 append :linked_files, '.env.production'
